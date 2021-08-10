c1c6ecb40425db106ca71eb213f8c791efdc7209
lzz <lzz@lzz.com>
2021-08-10 14:53:24 +0800

    第一次提交java代码

---
+++ src/Person.java
@@ -0,0 +1,7 @@
+/*** @Auther: Áõ×³×³
+ * @Date: 2021/8/10 
+ * @Description: PACKAGE_NAME
+ * @version: 1.0
+ */
+public class Person {
+}
---
+++ src/Users.java
@@ -0,0 +1,20 @@
+import java.util.Date;
+
+
+public class Users {
+    private int userin;
+    private String username;
+
+    public Users(int userin, String username) {
+        this.userin = userin;
+        this.username = username;
+    }
+
+    public Users(int userin) {
+        this.userin = userin;
+    }
+
+    public Users() {
+    }
+
+}
